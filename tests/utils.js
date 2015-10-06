38010a8a4619a4e163d3fe0099d50ac614e90bfd
Jerel Unruh <mail@unruhdesigns.com>
2015-10-06 12:36:23 -0500

    Enable long polling and re-enable safari

--- tests/utils.js
+++ tests/utils.js
@@ -6,6 +6,7 @@ export function pause(assert, milliseconds=5000) {
   // don't let the test hang if the server never responds
   assert.abortTimeout = window.setTimeout(() => {
     assert.ok(false, `The server did not respond within ${milliseconds / 1000}  seconds`)
+    QUnit.start()
   }, milliseconds)
 
 }
